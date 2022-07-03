071ceaf73fddeadcf7585cde55f3896280ae97fd
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-07-03 12:55:58 +0300

    added methods logic

--- FirstProject/Ocean.cs
+++ FirstProject/Ocean.cs
@@ -9,31 +9,24 @@ namespace FirstProject
         private const int DefaultNumPrey = 150;
         private const int DefaultNumPredators = 20;
         private const int DefaultNumObstacles = 75;
+        private const int DefaultNumOperations = 1000;
 
         private int numRows;
         private int numColumns;
         private int numPrey;
         private int numPredators;
         private int numObstacles;
+        private int numOperations;
         private Random random = new Random();
         private Cell[,] cells;
         private int size;
 
-        public Ocean(int numRows, int numColumns, int numPrey, int numPredators, int numObstacles)
+        public Ocean()
         {
-            this.numRows = numRows;
-            this.numColumns = numColumns;
-            this.numPrey = numPrey;
-            this.numPredators = numPredators;
-            this.numObstacles = numObstacles;
-
             Initialize();
         }
 
-        private void Initialize() {
-            size = this.numRows * this.numColumns;
-            cells = new Cell[this.numRows, this.numColumns];
-
+        public void Initialize() {
             InitCells();
         }
 
@@ -44,7 +37,7 @@ namespace FirstProject
         private void setNumRows(int rowsCount) {
             if (rowsCount > 25 || rowsCount < 0)
             {
-                numRows = DefaultNumColumns;
+                numRows = DefaultNumRows;
                 throw new InvalidInputException();
             }
             else
@@ -76,7 +69,7 @@ namespace FirstProject
             return numPrey;
         }
 
-        private void setPreyCount(int preyCount)
+        private void setNumPrey(int preyCount)
         {
             if (preyCount < 0 || preyCount > size - numObstacles)
             {
@@ -94,7 +87,7 @@ namespace FirstProject
             return numPredators;
         }
 
-        private void setPredatorsCount(int predatorsCount)
+        private void setNumPredators(int predatorsCount)
         {
             if (predatorsCount < 0 || predatorsCount > size - numObstacles)
             {
@@ -114,7 +107,7 @@ namespace FirstProject
         private void setNumObstacles(int obstaclesCount) {
             if (obstaclesCount > size || obstaclesCount < 0)
             {
-                numObstacles = size;
+                numObstacles = DefaultNumObstacles;
                 throw new InvalidInputException();
             }
             else
@@ -123,26 +116,45 @@ namespace FirstProject
             }
         }
 
+        public int getNumOperations() {
+            return numOperations;
+        }
+
+        private void setNumOperations(int numOperations) {
+            if (numOperations < 0 || numOperations > 1000) {
+                this.numOperations = DefaultNumOperations;
+                throw new InvalidInputException();
+            }
+        }
+
+        public Cell[,] getCells() {
+            return cells;
+        }
+
+        public Random getRandom() {
+            return random;
+        }
+
         private void InitCells()
         {
             AddEmptyCells();
 
             try
             {
-                Console.WriteLine("Enter the number of obstacles: ");
-                numObstacles = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the number of obstacles: ");
+                setNumObstacles(Convert.ToInt32(Console.ReadLine()));
 
-                Console.WriteLine("Enter the number of predators: ");
-                numPredators = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the number of predators: ");
+                setNumPredators(Convert.ToInt32(Console.ReadLine()));
 
-                Console.WriteLine("Enter the number of prey: ");
-                numPrey = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the number of prey: ");
+                setNumPrey(Convert.ToInt32(Console.ReadLine()));
             }
             catch (InvalidInputException exception) {
                 Console.WriteLine(exception.getMessage());
             }
 
-            Console.WriteLine("Number of obstacles is " + numObstacles);
+            Console.WriteLine("\nNumber of obstacles is " + numObstacles);
             Console.WriteLine("Number of predators is " + numPredators);
             Console.WriteLine("Number of prey is " + numPrey);
 
@@ -150,20 +162,34 @@ namespace FirstProject
             AddPredators();
             AddPrey();
 
-            DisplayStats(-1);
+            DisplayStats(0);
             DisplayCells();
             DisplayBorder();
         }
 
         private void AddEmptyCells()
         {
+            try
+            {
+                Console.Write("Enter the count of rows: ");
+                setNumRows(Convert.ToInt32(Console.ReadLine()));
+
+                Console.Write("Enter the count of columns: ");
+                setNumColumns(Convert.ToInt32(Console.ReadLine()));
+            }
+            catch (InvalidInputException exception) {
+                Console.WriteLine(exception.getMessage());
+            }
+
+            cells = new Cell[numRows, numColumns];
+
+            size = numColumns * numRows;
+
             for (int row = 0; row < numRows; row++)
             {
                 for (int column = 0; column < numColumns; column++)
                 {
-                    Cell cell = new Cell(new Coordinate(column, row));
-                    cells[row, column] = cell;
-                    cell.owner = this;
+                    cells[row, column] = new Cell(new Coordinate(row, column));
                 }
             }
         }
@@ -174,7 +200,7 @@ namespace FirstProject
             for (int i = 0; i < numObstacles; i++)
             {
                 emptyCell = GetEmptyCellCoord();
-                cells[emptyCell.Y, emptyCell.X] = new Obstacle(emptyCell);
+                cells[emptyCell.X, emptyCell.Y] = new Obstacle(emptyCell);
             }
         }
 
@@ -184,7 +210,7 @@ namespace FirstProject
             for (int i = 0; i < numPredators; i++)
             {
                 emptyCell = GetEmptyCellCoord();
-                cells[emptyCell.Y, emptyCell.X] = new Predator(emptyCell);
+                cells[emptyCell.X, emptyCell.Y] = new Predator(emptyCell);
             }
         }
 
@@ -194,44 +220,97 @@ namespace FirstProject
             for (int i = 0; i < numPrey; i++)
             {
                 emptyCell = GetEmptyCellCoord();
-                cells[emptyCell.Y, emptyCell.X] = new Prey(emptyCell);
+                cells[emptyCell.X, emptyCell.Y] = new Prey(emptyCell);
             }
         }
 
         private Coordinate GetEmptyCellCoord()
         {
-            int y = random.Next(0, numRows - 1);
-            int x = random.Next(0, numColumns - 1);
+            int y = random.Next(0, numColumns - 1);
+            int x = random.Next(0, numRows - 1);
 
             Coordinate emptyCell;
 
-            while (cells[y, x].image != '-') {
-                y = random.Next(0, numRows - 1);
-                x = random.Next(0, numColumns - 1);
+            while (cells[x, y].getImage() != '-') {
+                y = random.Next(0, numColumns - 1);
+                x = random.Next(0, numRows - 1);
             }
 
-            emptyCell = cells[y, x].offset;
+            emptyCell = cells[x, y].getOffset();
             return emptyCell;
         }
 
         private void DisplayBorder()
         {
-
+            for (int column = 0; column < numColumns; column++)
+            {
+                if (column == numColumns - 1)
+                {
+                    Console.WriteLine();
+                }
+                else if (column == 0)
+                {
+                    Console.WriteLine();
+                    Console.Write(new String(' ', 30));
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
         }
 
         private void DisplayCells()
         {
-
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int column = 0; column < numColumns; column++)
+                {
+                    cells[row, column].Display();
+                }
+            }
         }
 
         private void DisplayStats(int iteration)
         {
+            Console.WriteLine("\nIteration number: " + iteration++);
+            Console.WriteLine("Number of obstacles: " + numObstacles);
+            Console.WriteLine("Number of predators: " + numPredators);
+            Console.WriteLine("Number of prey: " + numPrey);
 
+            DisplayBorder();
         }
 
         public void Run()
         {
+            try
+            {
+                Console.WriteLine("Enter the number of operations: ");
+                setNumOperations(Convert.ToInt32(Console.ReadLine()));
+            }
+            catch (InvalidInputException exception) {
+                Console.WriteLine(exception.getMessage());
+            }
 
+            Console.WriteLine("The number of operations: " + numOperations);
+
+            for (int operation = 0; operation < numOperations; operation++) { 
+                if (numPredators > 0 && numPrey > 0)
+                {
+                    for (int row = 0; row < numRows; row++)
+                    {
+                        for (int column = 0; column < numColumns; column++)
+                        {
+                            cells[row, column].Process();
+                        }
+                    }
+
+                    DisplayStats(operation);
+                    DisplayCells();
+                    DisplayBorder();
+                }
+            }
         }
     }
 }
