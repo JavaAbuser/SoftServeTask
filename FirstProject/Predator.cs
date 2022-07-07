df46140fc69c6066b0455526cbc0e3f19f98f75d
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-07-07 16:24:25 +0300

    added methods Process and Reproduce

--- FirstProject/Predator.cs
+++ FirstProject/Predator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,62 @@ namespace FirstProject
 {
     internal class Predator : Prey
     {
-        public Predator() {
-            image = 'S';
+        private const int DefaultTimeToFeed = 6;
+        private const int TimeToReproduce = 6;
+
+        private int timeToFeed;
+
+        public Predator(Coordinate coordinate, int timeToFeed = DefaultTimeToFeed) : base(coordinate)
+        {
+            setImage('S');
+            this.timeToFeed = timeToFeed;
+        }
+
+        public override void Process()
+        {
+            Coordinate shouldBeEatenPreyCoordinate = getPreyCoord();
+            Coordinate predatorCoordinate = GetEmptyNeighborCoord();
+
+            if (timeToFeed <= 0)
+            {
+                AssignCellAt(getOffset(), new Cell(getOffset()));
+
+                ocean.numPredators--;
+            }
+            else if (timeToReproduce <= 0) {
+                Reproduce(predatorCoordinate);
+            }
+
+            else
+            {
+                if ((shouldBeEatenPreyCoordinate.getX() != getOffset().getX() || shouldBeEatenPreyCoordinate.getY() != getOffset().getY())
+                    || (shouldBeEatenPreyCoordinate.getX() != getOffset().getX() & shouldBeEatenPreyCoordinate.getY() != getOffset().getY()))
+                {
+                    int numPrey = ocean.getNumPrey();
+                    int numPreyEaten = ocean.getNumPreyEaten();
+
+                    ocean.numPrey--;
+                    ocean.setNumPreyEaten(numPreyEaten++);
+
+                    timeToFeed = DefaultTimeToFeed;
+                    timeToReproduce = TimeToReproduce;
+
+                    MoveFrom(getOffset(), shouldBeEatenPreyCoordinate);
+                }
+
+                else
+                {
+                    MoveFrom(getOffset(), predatorCoordinate);
+                    timeToReproduce--;
+                    timeToFeed--;
+                }
+            }
+        }
+
+        protected override void Reproduce(Coordinate coordinate)
+        {
+            ocean.numPredators++;
+            AssignCellAt(coordinate, new Predator(coordinate));
         }
     }
 }
