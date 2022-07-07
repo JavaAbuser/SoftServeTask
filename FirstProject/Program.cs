2cd664e831ca3bc1af98cff134e7950240cabd3d
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-07-07 16:31:29 +0300

    created object ocean and called method Run

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,8 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
+            Ocean ocean = new Ocean();
+            ocean.Run();
         }
     }
 }
