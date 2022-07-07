5fdd8cfb23f7399ba4ce97ab7d2469abef0a436b
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-07-07 16:30:41 +0300

    initial commit

---
+++ FirstProject/InvalidInputException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class InvalidInputException : Exception
+    {
+       private static string message = "You are out of range. We will set the default values.";
+       public InvalidInputException() : base(message) {
+       }
+
+       public string getMessage() {
+           return message;
+       }
+    }
+}
